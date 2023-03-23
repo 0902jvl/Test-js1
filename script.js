3d80ea7eb1143a2814044a75bc6e4fe1ca0cb001
Fin <0902jvl@gmail.com>
2023-03-23 16:53:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let int = 1555;
+// int++;
+// prompt('do you');
+
+// let x = 128;
+// // x = 5 + 1;
+// // x += 1;
+// // x++
+// console.log(x);
+
+// confirm('Вам есть 18 лет?')
+
+let answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Какая ваша фамиля?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
