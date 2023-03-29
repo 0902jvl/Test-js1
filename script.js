816acfefdb4ad268bce176e243af49c7e0b32bb1
Fin <0902jvl@gmail.com>
2023-03-29 16:44:05 +0300

    cycle

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ const d = prompt("На сколько его оцените?", "");
 personalMovidDB.movies[a] = b;
 personalMovidDB.movies[c] = d;
 
-console.log(personalMovidDB);
-
 // int++;
 // prompt('do you');
 
@@ -35,3 +33,124 @@ console.log(personalMovidDB);
 // answers[2] = prompt("Сколько вам лет?", "");
 
 // document.write(answers);
+
+// 21 Logical operators
+console.log(personalMovidDB);
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+    console.log("Done!");
+}
+
+
+// 22 a cycle
+while (num < 10) {
+    console.log(num);
+    num += 2;
+}
+
+let num = 1;
+do {
+    console.log(num);
+    num += 3;
+}
+while (num < 10);
+
+for (let i = 1; i < 10; i += 2) {
+    if (i > 6) {
+        break;
+    }
+    console.log(i);
+}
+
+// 23 cycle within a cycle
+let resalt = "";
+const len = 7;
+
+for (let i = 0; i < len; i++) {
+
+    for (let j = 0; j < i; j++) {
+        resalt += "*";
+    }
+    resalt += "\n";
+}
+console.log(resalt);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`firsd level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`       second level: ${j}`);
+        for (let k = 0; k < 7; k++) {
+            if (k === 6) break first;
+            console.log(`                therd level: ${k}`);
+        }
+    }
+}
+
+
+// Упражнение по написанию кода 3: Задания на использование
+
+//                                     Место для первой задачи
+for (let i = 5; i < 11; i++) {
+    console.log(i);
+}
+//                                      Место для второй задачи
+for (let i = 20; i > 9; i--) {
+    if (i === 13) break;
+    console.log(i);
+}
+//                                      Место для третьей задачи
+for (let i = 2; i < 11; i += 2) {
+    console.log(i);
+}
+//                                      Место для четвертой задачи
+let i = 2;
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++
+}
+//                                      Место для пятой задачи
+const arrayOfNumbers = [];
+// Пишем решение вот тут
+for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = 1;
+}
+console.log(arrayOfNumbers);
+
+
+//1
+const arr = [3, 5, 8, 16, 20, 23, 55];
+const result = [];
+for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+}
+console.log(result);
+
+//2
+const data = [5, 10, "Shopping", 20, "Homework"];
+for (let i = 0; i < data.length; i++) {
+    if (typeof (data[i]) === "number") {
+        data[i] = data[i] * 2;
+    } else if (typeof (data[i]) === "string") {
+        data[i] = `${data[i]}- data`;
+    }
+}
+console.log(data);
+
+//3
+const data2 = [5, 10, "Shopping", 20, "Homework"];
+const result2 = [];
+for (let i = 1; i < data2.length; i++) {
+    result2[i - 1] = data[data2.length - i];
+}
+console.log(result2);
+
+
